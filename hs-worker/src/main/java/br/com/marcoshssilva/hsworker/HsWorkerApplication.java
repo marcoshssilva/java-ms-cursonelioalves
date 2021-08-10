9781d2e0544c48b18e8b41c8b2dac817f07a22bb
Marcos Henrique <76234459+marcoshs1998@users.noreply.github.com>
2021-08-10 10:01:53 -0300

    projeto HR-WORKER gerado

---
+++ hs-worker/src/main/java/br/com/marcoshssilva/hsworker/HsWorkerApplication.java
@@ -0,0 +1,13 @@
+package br.com.marcoshssilva.hsworker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HsWorkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HsWorkerApplication.class, args);
+	}
+
+}
---
+++ hs-worker/src/test/java/br/com/marcoshssilva/hsworker/HsWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marcoshssilva.hsworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HsWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
