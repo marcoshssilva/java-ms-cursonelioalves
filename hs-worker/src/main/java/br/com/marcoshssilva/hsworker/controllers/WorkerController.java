ab92706041d99b344a1690778768f648e39e5924
Marcos Henrique <76234459+marcoshs1998@users.noreply.github.com>
2021-08-22 00:19:50 -0300

    Ribbon Load Balance apply in HR-WORKERS and HR-PAYROLL

--- hs-worker/src/main/java/br/com/marcoshssilva/hsworker/controllers/WorkerController.java
+++ hs-worker/src/main/java/br/com/marcoshssilva/hsworker/controllers/WorkerController.java
@@ -2,7 +2,10 @@ package br.com.marcoshssilva.hsworker.controllers;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,17 +19,25 @@ import br.com.marcoshssilva.hsworker.repositories.WorkerRepository;
 @RequestMapping("/workers")
 public class WorkerController {
 
+	private static Logger LOG = LoggerFactory.getLogger(WorkerController.class);
+	
+	@Autowired
+	private Environment env;
+	
 	@Autowired
 	WorkerRepository workerRepository;
 	
 	@RequestMapping(method = RequestMethod.GET)
 	public ResponseEntity<List<Worker>> findAll(){
-		return ResponseEntity.ok(this.workerRepository.findAll());
+		List<Worker> workers = this.workerRepository.findAll();
+		LOG.info("HR-WORKER - Successfull request, PORT=" + env.getProperty("local.server.port"));
+		return ResponseEntity.ok(workers);
 	}
 	
 	@RequestMapping(method = RequestMethod.GET, value = "/{id}")
 	public ResponseEntity<Worker> findById(@PathVariable Long id){
 		Worker w = workerRepository.findById(id).get();
+		LOG.info("HR-WORKER - Successfull request, PORT=" + env.getProperty("local.server.port"));
 		return ResponseEntity.ok(w);
 	}
 }
