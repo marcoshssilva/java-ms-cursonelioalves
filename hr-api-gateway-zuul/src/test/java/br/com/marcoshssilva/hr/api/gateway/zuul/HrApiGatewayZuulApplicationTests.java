bf9eb151ed368a58820feb4c9af14faec6fa2dd0
Marcos Henrique <76234459+marcoshs1998@users.noreply.github.com>
2021-08-24 10:15:44 -0300

    HR-API-GATEWAY-ZUUL has been created

---
+++ hr-api-gateway-zuul/src/test/java/br/com/marcoshssilva/hr/api/gateway/zuul/HrApiGatewayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marcoshssilva.hr.api.gateway.zuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrApiGatewayZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
