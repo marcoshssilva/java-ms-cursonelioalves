ab64babca8ec6f382c5c2ef7493e4f4e0dc71620
Marcos Henrique <76234459+marcoshs1998@users.noreply.github.com>
2021-08-25 10:37:52 -0300

    Config to connect to a private repository on GitHub

--- hr-config-server/src/main/java/br/com/marcoshssilva/hrconfigserver/HrConfigServerApplication.java
+++ hr-config-server/src/main/java/br/com/marcoshssilva/hrconfigserver/HrConfigServerApplication.java
@@ -1,15 +1,33 @@
 package br.com.marcoshssilva.hrconfigserver;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
 @SpringBootApplication
-public class HrConfigServerApplication {
+public class HrConfigServerApplication implements CommandLineRunner {
 
+	@Value("${spring.cloud.config.server.git.username}")
+	private String git_user;
+	
+	@Value("${spring.cloud.config.server.git.password}")
+	private String git_pass;
+	
+	private static final Logger LOG = LoggerFactory.getLogger(HrConfigServerApplication.class);
+	
 	public static void main(String[] args) {
 		SpringApplication.run(HrConfigServerApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		LOG.info("CONFIG SERVER enable and connected to Git");
+		//LOG.info("CREDENTIALS: " + git_user + " " + git_pass);
 	}
 
 }
